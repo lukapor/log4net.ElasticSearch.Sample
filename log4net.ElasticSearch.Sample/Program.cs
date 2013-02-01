ca6442cb7dacf97968cf3bece17771ed79195bfb
JP Toto <james.p.toto@gmail.com>
2013-01-31 19:06:02 -0500

    Initial commit

---
+++ log4net.ElasticSearch.Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace log4net.ElasticSearch.Sample
+{
+    class Program
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            _log.Info("log4net.ElsaticSearch Test");
+            Console.WriteLine("Added a record.");
+            Console.ReadKey();
+        }
+    }
+}
